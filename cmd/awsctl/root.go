25486765fd7353a5f711e4b4e588b662a05683ce
sanjog-lama <sanjog.gomden@berrybytes.com>
2025-03-05 17:16:09 +0545

    "feat: "Added init setup to check if aws cli is installed or not"

--- cmd/awsctl/root.go
+++ cmd/awsctl/root.go
@@ -1,6 +1,7 @@
 package awsctl
 
 import (
+	"awsctl/internal/aws"
 	"fmt"
 	"os"
 
@@ -8,12 +9,18 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "your-app-name",
+	Use:   "awsctl",
 	Short: "A CLI tool for AWS operations",
-	Long:  `A Golang-based CLI for AWS tasks such as EKS, RDS, and SSO authentication.`,
+	Long:  `A Golang-based CLI for AWS tasks such as EKS, RDS, bastian and SSO authentication.`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Checking if AWS CLI is installed...")
+		aws.InitAWSSetup()
+	},
+	// Define the Run function to show help when no subcommand is provided
 	Run: func(cmd *cobra.Command, args []string) {
+		// Print a message before showing the help
 		fmt.Println("No subcommand provided. Showing help...")
-		cmd.Help()
+		cmd.Help() // Display the help message for the root command
 	},
 }
 
---
+++ internal/aws/setup.go
@@ -0,0 +1,38 @@
+package aws
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+)
+
+// Initialize AWS setup
+func InitAWSSetup() {
+	handleSignals()
+	checkAWSCLI()
+}
+
+// handleSignals ensures child processes are cleaned up on exit
+func handleSignals() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		fmt.Println("Received termination signal. Cleaning up...")
+		os.Exit(1)
+	}()
+}
+
+// checkAWSCLI ensures AWS CLI is installed
+func checkAWSCLI() {
+	_, err := exec.LookPath("aws")
+	if err != nil {
+		fmt.Println("AWS CLI not found:", err)
+		os.Exit(1)
+	} else {
+		fmt.Println("AWS CLI is installed and available in PATH.")
+	}
+}
