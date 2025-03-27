3a0d4aaface5e1597e872addb017f5a074e18500
sanjog-lama <sanjog.gomden@berrybytes.com>
2025-03-27 09:29:53 +0545

    fix: removed unused prompt functions

--- utils/prompt/prompt.go
+++ utils/prompt/prompt.go
@@ -3,8 +3,6 @@ package promptutils
 import (
 	"errors"
 	"fmt"
-	"os"
-	"os/exec"
 	"strings"
 
 	"github.com/manifoldco/promptui"
@@ -12,11 +10,6 @@ import (
 
 type Prompter interface {
 	PromptForSelection(label string, items []string) (string, error)
-	PromptSSOConfiguration() (string, string, string, error)
-	PromptForRegion() (string, error)
-	PromptForRole() (string, error)
-	PromptForAccount() (string, error)
-	PromptForProfile() string
 	PromptForConfirmation(prompt string) bool
 }
 
@@ -51,108 +44,6 @@ func (p *RealPrompter) PromptForSelection(label string, items []string) (string,
 	}
 
 	return selected, nil
-}
-
-func (p *RealPrompter) PromptSSOConfiguration() (string, string, string, error) {
-	prompt := promptui.Prompt{
-		Label: "Enter Profile Name",
-	}
-	profileName, err := prompt.Run()
-	if err != nil {
-		return "", "", "", fmt.Errorf("failed to get profile name: %v", err)
-	}
-
-	prompt = promptui.Prompt{
-		Label: "Enter SSO Start URL",
-	}
-	ssoStartURL, err := prompt.Run()
-	if err != nil {
-		return "", "", "", fmt.Errorf("failed to get SSO start URL: %v", err)
-	}
-
-	prompt = promptui.Prompt{
-		Label: "Enter SSO Region",
-	}
-	ssoRegion, err := prompt.Run()
-	if err != nil {
-		return "", "", "", fmt.Errorf("failed to get SSO region: %v", err)
-	}
-
-	return profileName, ssoStartURL, ssoRegion, nil
-}
-
-func (p *RealPrompter) PromptForRegion() (string, error) {
-	regions := []string{"us-east-1", "us-west-2", "eu-central-1"}
-	prompt := promptui.Select{
-		Label: "Choose region",
-		Items: regions,
-		Size:  len(regions),
-	}
-	_, selectedRegion, err := prompt.Run()
-	if err != nil {
-		return "", fmt.Errorf("selection aborted")
-	}
-	return selectedRegion, nil
-}
-
-func (p *RealPrompter) PromptForRole() (string, error) {
-	roles := []string{"AdministratorAccess", "Billing", "PowerUserAccess", "ViewOnlyAccess", "LogsReadOnlyPermissionSet", "S3FullAccess"}
-	prompt := promptui.Select{
-		Label: "Choose role",
-		Items: roles,
-		Size:  len(roles),
-	}
-	_, selectedRole, err := prompt.Run()
-	if err != nil {
-		return "", fmt.Errorf("selection aborted")
-	}
-	return selectedRole, nil
-}
-
-func (p *RealPrompter) PromptForAccount() (string, error) {
-	accounts := []string{"Logs", "NonProd", "Security", "Production", "Shared Services", "MarcRosenberg"}
-	prompt := promptui.Select{
-		Label: "Choose account",
-		Items: accounts,
-		Size:  len(accounts),
-	}
-	_, selected, err := prompt.Run()
-	if err != nil {
-		return "", fmt.Errorf("selection aborted")
-	}
-	return selected, nil
-}
-
-func (p *RealPrompter) PromptForProfile() string {
-	cmd := exec.Command("aws", "configure", "list-profiles")
-	output, err := cmd.Output()
-	if err != nil {
-		fmt.Println("Error: AWS CLI not found or misconfigured.")
-		os.Exit(1)
-	}
-
-	profiles := strings.Split(strings.TrimSpace(string(output)), "\n")
-	if len(profiles) == 0 || profiles[0] == "" {
-		return ""
-	}
-
-	prompt := promptui.Select{
-		Label: "Choose AWS Profile",
-		Items: profiles,
-		Size:  15,
-	}
-
-	_, selectedProfile, err := prompt.Run()
-	if err != nil {
-		fmt.Println("Profile selection cancelled.")
-		os.Exit(1)
-	}
-
-	os.Setenv("AWS_PROFILE", selectedProfile)
-	fmt.Println("Run below command in your shell to persist the profile:")
-	fmt.Printf("export AWS_PROFILE=%s\n", selectedProfile)
-
-	return selectedProfile
 }
 
 func (p *RealPrompter) PromptForConfirmation(prompt string) bool {
