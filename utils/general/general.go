e97d547e7c406e9d6c7bd97cf1ea791abd292b7b
sanjog-lama <sanjog.gomden@berrybytes.com>
2025-06-05 17:37:58 +0545

    fix: fixed strict checking of region when user is authenticated

--- utils/general/general.go
+++ utils/general/general.go
@@ -9,6 +9,7 @@ import (
 	"regexp"
 	"syscall"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 )
@@ -62,37 +63,29 @@ func NewGeneralUtilsManager() GeneralUtilsInterface {
 	return &DefaultGeneralUtilsManager{}
 }
 
-func isValidAWSRegion(region string) bool {
-	ctx := context.Background()
-
-	cfg, err := config.LoadDefaultConfig(ctx)
-	if err != nil {
-		return false
-	}
-
-	ec2Client := ec2.NewFromConfig(cfg)
-
-	output, err := ec2Client.DescribeRegions(ctx, &ec2.DescribeRegionsInput{})
-	if err != nil {
-		return false
-	}
-
-	for _, r := range output.Regions {
-		if r.RegionName != nil && *r.RegionName == region {
-			return true
-		}
-	}
-	return false
-}
-
 func isValidRegionFormat(region string) bool {
 	// Matches patterns like us-east-1, ap-southeast-2
 	return regexp.MustCompile(`^[a-z]{2}-[a-z]+-\d+$`).MatchString(region)
 }
 
 func IsRegionValid(region string) bool {
-	if isValidAWSRegion(region) {
-		return true
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err == nil {
+		ec2Client := ec2.NewFromConfig(cfg)
+		output, err := ec2Client.DescribeRegions(ctx, &ec2.DescribeRegionsInput{
+			AllRegions: aws.Bool(true),
+		})
+		if err == nil {
+			for _, r := range output.Regions {
+				if r.RegionName != nil && *r.RegionName == region {
+					return true
+				}
+			}
+			return false
+		}
 	}
+
 	return isValidRegionFormat(region)
 }
